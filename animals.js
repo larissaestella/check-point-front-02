aed96d4c7c02d8b3d34598bd328dcd94310c8491
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-27 08:50:07 -0300

    Subindo arquivos em JavaScript

---
+++ animals.js
@@ -0,0 +1,9 @@
+const animals = ["macaco", "tucano", "elefante", "pavão", "hipopótamo", "leão"];
+
+if (!(animals.includes("leão"))){
+    console.log("Leão não existe no array animals.")
+}
+
+else {
+    console.log("Existe leão no array animals.")
+}
---
+++ series.js
@@ -0,0 +1,8 @@
+const my3favoritetvshows = ["Game of Thrones", "Gilmore Girls", "Peaky Blinders"];
+
+if (my3favoritetvshows.length === 3){
+    console.log("true")
+}
+else {
+    console.log("false")
+}
